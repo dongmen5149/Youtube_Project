5f55581f3a9461c32b816af3d03900212d716e8b
Ryu <78216411+dongmen5149@users.noreply.github.com>
2021-06-29 01:07:25 +0900

    수정

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const port = 3000
 const { User } = require('./models/User')
 const bodyParser = require('body-parser')
 const config = require('./config/key')
+const cookieParser = require('cookie-parser')
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -34,7 +36,38 @@ app.post("/register", (req, res) => {
 
 })
 
+app.post('/api/users/login', (req, res) => {
+  // 요청된 이메일을 데이터 베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        messagee: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀 번호인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, messagee: "비밃번호가 틀렸습니다." })
+
+      // 비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지 등등
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
+    })
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
 
