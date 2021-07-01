6ca1d8b761851807ed373a6f636b7f80746b1cb8
Ryu <78216411+dongmen5149@users.noreply.github.com>
2021-07-01 23:15:23 +0900

    프론트파트

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 const { User } = require('./models/User')
 const bodyParser = require('body-parser')
 const config = require('./config/key')
@@ -20,6 +20,10 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+  
+app.get('/api/hello', (req,res) =>{
+  res.send("안녕하세요~")
+})
 
 app.get('/', (req, res) => res.send('처음이댜'))
 
